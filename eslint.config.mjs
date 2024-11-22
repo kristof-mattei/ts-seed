44bb6873b02796ed801368f2327abee2fae8899c
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2024-11-22 16:27:00 -0700

    fix: ensure our basic rules win over love

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,72 @@ import promise from "eslint-plugin-promise";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import tseslint from "typescript-eslint";
 
+const sharedRules = {
+    "arrow-body-style": ["error", "always"],
+
+    curly: ["error", "all"],
+    "eol-last": ["error", "always"],
+    eqeqeq: ["error", "always"],
+
+    "max-len": ["off"],
+    "no-extra-semi": ["off"],
+    "no-param-reassign": ["off"],
+    "no-restricted-imports": [
+        "error",
+        {
+            patterns: [".*"],
+        },
+    ],
+    "no-restricted-syntax": ["error", "DebuggerStatement", "LabeledStatement", "WithStatement"],
+    "no-return-await": ["error"],
+    "no-shadow": ["error"],
+    "no-underscore-dangle": ["off"],
+    "no-unused-expressions": ["error"],
+    "no-useless-constructor": ["off"],
+    "object-shorthand": ["error", "always"],
+    "prefer-template": ["error"],
+    quotes: [
+        "error",
+        "double",
+        {
+            allowTemplateLiterals: false,
+            avoidEscape: true,
+        },
+    ],
+    "require-await": ["error"],
+    "sort-imports": [
+        "error",
+        {
+            ignoreDeclarationSort: true,
+        },
+    ],
+
+    "sort-keys": ["off"],
+    "unicorn/no-null": ["off"],
+    "unicorn/prefer-ternary": ["off"],
+
+    "import/extensions": [
+        "error",
+        "never",
+        {
+            json: "always",
+        },
+    ],
+    "import/newline-after-import": ["error"],
+    "import/no-cycle": ["off"],
+    "import/no-extraneous-dependencies": ["off"],
+    "import/no-relative-packages": ["error"],
+    "import/no-unresolved": ["error"],
+    "import/order": [
+        "error",
+        {
+            alphabetize: { caseInsensitive: true, order: "asc" },
+            "newlines-between": "always-and-inside-groups",
+        },
+    ],
+    "import/prefer-default-export": ["off"],
+};
+
 export default tseslint.config(
     js.configs.recommended,
     {
@@ -45,80 +111,12 @@ export default tseslint.config(
             ...importPlugin.configs.recommended.rules,
             ...eslintPluginUnicorn.configs.recommended.rules,
 
-            "import/export": ["error"],
-            "import/first": ["error"],
-            "import/no-absolute-path": ["error", { esmodule: true, commonjs: true, amd: false }],
-            "import/no-duplicates": ["error"],
-            "import/no-named-default": ["error"],
-            "import/no-webpack-loader-syntax": ["error"],
-            "arrow-body-style": ["error", "always"],
-
-            curly: ["error", "all"],
-            "eol-last": ["error", "always"],
-            eqeqeq: ["error", "always"],
-
-            "max-len": ["off"],
-            "no-dupe-keys": ["warn"],
-            "no-extra-semi": ["off"],
-            "no-param-reassign": ["off"],
-            "no-restricted-imports": [
-                "error",
-                {
-                    patterns: [".*"],
-                },
-            ],
-            "no-restricted-syntax": ["error", "DebuggerStatement", "LabeledStatement", "WithStatement"],
-            "no-return-await": ["error"],
-            "no-shadow": ["error"],
-            "no-underscore-dangle": ["off"],
-            "no-unused-expressions": ["error"],
-            "no-useless-constructor": ["off"],
-            "object-shorthand": ["error", "always"],
-            "prefer-const": ["error"],
-            "prefer-template": ["error"],
-            quotes: [
-                "error",
-                "double",
-                {
-                    allowTemplateLiterals: false,
-                    avoidEscape: true,
-                },
-            ],
-            "require-await": ["error"],
-            "sort-imports": [
-                "error",
-                {
-                    ignoreDeclarationSort: true,
-                },
-            ],
-
-            "sort-keys": ["off"],
-            "unicorn/no-null": ["off"],
-            "unicorn/prefer-ternary": ["off"],
-
-            "import/extensions": [
-                "error",
-                "never",
-                {
-                    json: "always",
-                },
-            ],
-            "import/newline-after-import": ["error"],
-            "import/no-cycle": ["off"],
-            "import/no-extraneous-dependencies": ["off"],
-            "import/no-relative-packages": ["error"],
-            "import/no-unresolved": ["error"],
-            "import/order": [
-                "error",
-                {
-                    alphabetize: { caseInsensitive: true, order: "asc" },
-                    "newlines-between": "always-and-inside-groups",
-                },
-            ],
-            "import/prefer-default-export": ["off"],
+            ...sharedRules,
         },
     },
     ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
     {
         files: ["**/*.ts", "**/*.tsx"],
         ignores: ["**/*.mjs"],
@@ -153,9 +151,9 @@ export default tseslint.config(
         rules: {
             ...importPlugin.configs.typescript.rules,
             ...love.rules,
+            ...sharedRules,
 
             "@stylistic/ts/no-extra-semi": ["error"],
-            "@typescript-eslint/array-type": ["error", { default: "array" }],
 
             "@typescript-eslint/consistent-type-imports": [
                 "error",
@@ -164,51 +162,14 @@ export default tseslint.config(
                     fixStyle: "separate-type-imports",
                     prefer: "type-imports",
                 },
-            ],
-
+            ], // different than love
             "@typescript-eslint/prefer-destructuring": ["off"],
-
             "@typescript-eslint/explicit-member-accessibility": ["error"],
-
             "@typescript-eslint/explicit-module-boundary-types": ["error"],
-
-            "@typescript-eslint/member-ordering": [
-                "error",
-                {
-                    default: [
-                        // Index signature
-                        "signature",
-                        // Fields
-                        "private-field",
-                        "public-field",
-                        "protected-field",
-                        // Constructors
-                        "public-constructor",
-                        "protected-constructor",
-                        "private-constructor",
-                        // Methods
-                        "public-method",
-                        "protected-method",
-                        "private-method",
-                    ],
-                },
-            ],
-            "@typescript-eslint/naming-convention": [
-                "error",
-                // {
-                //     format: ["camelCase", "PascalCase", "UPPER_CASE"],
-                //     leadingUnderscore: "allow",
-                //     selector: "variableLike",
-                //     trailingUnderscore: "allow",
-                // },
-                {
-                    format: ["camelCase", "PascalCase", "UPPER_CASE"],
-                    selector: "enumMember",
-                },
-            ],
-            "@typescript-eslint/no-empty-object-type": ["error"],
-            "@typescript-eslint/no-explicit-any": ["error", { fixToUnknown: true, ignoreRestArgs: false }],
-            "@typescript-eslint/no-extraneous-class": ["error"],
+            "@typescript-eslint/member-ordering": ["error"],
+            "@typescript-eslint/no-empty-object-type": ["error"], // stricter than love
+            "@typescript-eslint/no-explicit-any": ["error", { fixToUnknown: true, ignoreRestArgs: false }], // stricter than love
+            "@typescript-eslint/no-extraneous-class": ["error"], // stricter than love
             "@typescript-eslint/no-magic-numbers": ["off"],
             "@typescript-eslint/no-shadow": ["error"],
             "@typescript-eslint/no-unused-expressions": [
@@ -219,7 +180,7 @@ export default tseslint.config(
                     allowTernary: false,
                     enforceForJSX: false,
                 },
-            ],
+            ], // stricter than love
             "@typescript-eslint/no-unused-vars": [
                 "error",
                 {
@@ -229,31 +190,15 @@ export default tseslint.config(
                     ignoreRestSiblings: false,
                     vars: "all",
                 },
-            ],
+            ], // different than love
             "@typescript-eslint/parameter-properties": ["error"],
-            "@typescript-eslint/prefer-for-of": ["error"],
-
-            "@typescript-eslint/prefer-regexp-exec": ["warn"],
-            "@typescript-eslint/prefer-string-starts-ends-with": ["error"],
             "@typescript-eslint/promise-function-async": ["off"],
             "@typescript-eslint/require-await": ["error"],
 
-            "@typescript-eslint/unified-signatures": ["error"],
-
             "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
-
-            "n/handle-callback-err": ["error", "^(err|error)$"],
-            "n/no-callback-literal": ["error"],
-            "n/no-deprecated-api": ["error"],
-            "n/no-exports-assign": ["error"],
-            "n/no-new-require": ["error"],
-            "n/no-path-concat": ["error"],
-            "n/process-exit-as-throw": ["error"],
 
             "perfectionist/sort-intersection-types": ["error"],
             "perfectionist/sort-union-types": ["error"],
-
-            "promise/param-names": ["error"],
         },
     },
 
