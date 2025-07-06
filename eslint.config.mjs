91ce3bf62a3510b6ad33ce4d7165a26c5b1ee504
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-07-06 09:35:18 -0700

    fix: centralize configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -170,8 +170,7 @@ export default tseslint.config(
                     fixStyle: "separate-type-imports",
                     prefer: "type-imports",
                 },
-            ],
-            // different than love
+            ], // different than love
             "@typescript-eslint/prefer-destructuring": ["off"],
             "@typescript-eslint/explicit-member-accessibility": ["error"],
             "@typescript-eslint/explicit-module-boundary-types": ["error"],
