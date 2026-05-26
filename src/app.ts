81d4d769d2027c38fc9a29ce3eef902dcf4d99b0
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-05-26 14:17:08 -0700

    feat: fastify

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,127 @@
-import type { Express } from "express";
-import express from "express";
+import type { Static, TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { Type } from "@fastify/type-provider-typebox";
+import type {
+    FastifyBaseLogger,
+    FastifyInstance,
+    RawReplyDefaultExpression,
+    RawRequestDefaultExpression,
+    RawServerDefault,
+} from "fastify";
+import Fastify from "fastify";
 
 import { contents } from "./resources/solarSystem.json" with { type: "json" };
 
 import { getRandomIntInclusive } from "./utils/random";
 
-export function createApp(): Express {
-    const app = express();
+export type App = FastifyInstance<
+    RawServerDefault,
+    RawRequestDefaultExpression,
+    RawReplyDefaultExpression,
+    FastifyBaseLogger,
+    TypeBoxTypeProvider
+>;
 
-    app.get("/", (_request, response) => {
-        const world = contents[getRandomIntInclusive(0, contents.length - 1)];
+export function createApp(): App {
+    const app = Fastify().withTypeProvider<TypeBoxTypeProvider>();
 
-        if (world === undefined) {
-            throw new Error("something went horribly wrong");
-        }
-
-        response.status(200).send({
-            message: `Hello ${world}!`,
-        });
+    const Root = Type.Object({
+        message: Type.String(),
     });
+    type Root = Static<typeof Root>;
 
-    app.get("/fail", (_request, _response) => {
+    app.get(
+        "/",
+        {
+            schema: {
+                response: {
+                    200: Root,
+                },
+            },
+        },
+        (_request, _reply): Root => {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- we only get an index inside of `contents.length`.
+            const world = contents[getRandomIntInclusive(0, contents.length - 1)]!;
+
+            return { message: `Hello ${world}!` };
+        },
+    );
+
+    app.get("/fail", (_request, _reply) => {
         // to inspect stacktrace
         throw new Error("OMG WTF BBQ");
     });
 
-    app.get("/health", (_request, response) => {
-        response.status(200).send({
-            status: "ok",
-        });
+    const Health = Type.Object({
+        status: Type.Literal("ok"),
     });
+    type Health = Static<typeof Health>;
 
-    app.get("/f/:param", (request, response) => {
-        response.status(200).send({
-            status: "ok",
-            param_escaped: JSON.stringify(request.params.param),
-        });
-    });
+    app.get(
+        "/health",
+        {
+            schema: {
+                response: {
+                    200: Health,
+                },
+            },
+        },
+        (_request, _reply): Health => {
+            return { status: "ok" };
+        },
+    );
 
-    app.get("/f/:param1/blah/:param2", (request, response) => {
-        response.status(200).send({
-            status: "ok",
-            param1_escaped: JSON.stringify(request.params.param1),
-            param2_escaped: JSON.stringify(request.params.param2),
-        });
+    const FParameter = Type.Object({
+        status: Type.Literal("ok"),
+        param_escaped: Type.String(),
     });
+    type FParameter = Static<typeof FParameter>;
+
+    app.get(
+        "/f/:param",
+        {
+            schema: {
+                params: Type.Object({ param: Type.String() }),
+                response: {
+                    200: FParameter,
+                },
+            },
+        },
+        (request, _reply): FParameter => {
+            return {
+                status: "ok",
+                param_escaped: JSON.stringify(request.params.param),
+            };
+        },
+    );
+
+    const FParameterBlahParameter = Type.Object({
+        status: Type.Literal("ok"),
+        param1_escaped: Type.String(),
+        param2_escaped: Type.String(),
+    });
+    type FParameterBlahParameter = Static<typeof FParameterBlahParameter>;
+
+    app.get(
+        "/f/:param1/blah/:param2",
+        {
+            schema: {
+                params: Type.Object({
+                    param1: Type.String(),
+                    param2: Type.String(),
+                }),
+                response: {
+                    200: FParameterBlahParameter,
+                },
+            },
+        },
+        (request, _reply): FParameterBlahParameter => {
+            return {
+                status: "ok",
+                param1_escaped: JSON.stringify(request.params.param1),
+                param2_escaped: JSON.stringify(request.params.param2),
+            };
+        },
+    );
 
     return app;
 }
